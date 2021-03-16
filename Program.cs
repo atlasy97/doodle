f25953304a0ce80e949f39b046ab9abd5d32f8ec
Paraizs Attila <paraizsattila97@gmail.com>
2021-03-16 22:15:56 +0100

    This is a new commit for what I originally planned to be amended

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Doodle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
